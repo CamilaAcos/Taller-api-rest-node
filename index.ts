d4f2cbde952811ae9f7b325c2979b2f6e6409abe
CamilaAcos <camiacofer@gmail.com>
2024-10-16 10:16:00 -0500

    creacion index

---
+++ index.ts
@@ -0,0 +1,24 @@
+import Express from 'express'
+ 
+const createServer = () => {
+
+  const app = Express()
+
+  const PORT = process.env.PORT || 3000
+
+  app.get('/',(req, res)=>{
+
+    res.send('Hola mundo')
+
+  })
+
+  app.listen(PORT, ()=>{
+
+    console.log(`Server - API REST: http://localhost:${PORT}`);
+
+  })
+
+}
+ 
+createServer()
+ 
